16fb8dc0d2739be2257db121a86c5bf8192d16bd
Johnny Charvet <43849966+JoCharvet@users.noreply.github.com>
2023-08-22 11:27:00 +0200

    demander a l'utilisateur son nom et son prénom dans program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using System.Text.RegularExpressions;
+
+string name = "";
+string firstName = "";
+
+name = AskName();
+firstName = AskFirstName();
+
+string AskName()
+{
+    Console.WriteLine("Veuillez saisir votre nom :");
+    string name Console.ReadLine();
+
+    return Regex.IsMatch(name, @"^[\p{L}]{2,}$") ? name : AskName();
+
+}
+
+string AskFirstName()
+{
+    Console.WriteLine("Veuillez saisir votre prénom :");
+    string firstName = Console.ReadLine();
+
+    return Regex.IsMatch(firstName, @"^[\p{L}]{2,}$") ? firstName : AskFirstName();
+}
