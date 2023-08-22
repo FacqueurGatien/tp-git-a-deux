3defea259e147e44867fc9f28ed17a66143e6b87
Facqueur <104431014+FacqueurGatien@users.noreply.github.com>
2023-08-22 11:48:02 +0200

    FixError Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,11 @@
 
 using tpGitADeux;
-
-// See https://aka.ms/new-console-template for more information
-
 using System.Text.RegularExpressions;
-
-name = AskName();
-firstName = AskFirstName();
-
-Person myPerson = new Person(name,firstName);
-Console.WriteLine(myPerson.ToString());
 
 string AskName()
 {
     Console.WriteLine("Veuillez saisir votre nom :");
-    string name Console.ReadLine();
+    string name = Console.ReadLine();
 
     return Regex.IsMatch(name, @"^[\p{L}]{2,}$") ? name : AskName();
 
@@ -27,3 +18,8 @@ string AskFirstName()
 
     return Regex.IsMatch(firstName, @"^[\p{L}]{2,}$") ? firstName : AskFirstName();
 }
+
+Person myPerson = new Person(AskName(),AskFirstName());
+Console.WriteLine(myPerson.ToString());
+
+
