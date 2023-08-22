cb9446e0f38210883eb5de01a297f49f3378a69b
Facqueur <104431014+FacqueurGatien@users.noreply.github.com>
2023-08-22 11:26:00 +0200

    update program instanciate myperson

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpGitADeux;
+
+Person myPerson = new Person();
+Console.WriteLine(myPerson.ToString());
